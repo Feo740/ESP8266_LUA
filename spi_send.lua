8a0478cb37c2fb9e9c1209ad4e43bd0d0c1eaf7c
Feodor Terekhov <feo2006@yandex.ru>
2016-10-28 00:01:17 +0400

    Теперь  SPI передает 2 байта: первый - адрес ШИМ, второй  - значение. расчет значения и привязка адреса пока не реализована.

--- spi_send.lua
+++ spi_send.lua
@@ -1,9 +1,10 @@
 local T = {}
 do
   local function spi_send(d)
-    spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 80);
-    spi.send(1,d)
-    print('spi_send '..d)
+    spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 16, 80);
+    spi.set_mosi(1,0,8,d)
+    spi.transaction(1,0,0,8,3,8,0,0)
+    print('spi_send ')
         end
    
 T=spi_send   
